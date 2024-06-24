8f346504cb71cf6ca0fdd9b905f94f170f575485
Hemant kumar kaushik <ftd10622368@dseu.ac.in>
2024-06-24 20:31:14 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('.text', {
+        strings: ['Web Developer', 'Designer', 'Freelancer'],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true
+    });
+});
